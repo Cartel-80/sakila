87bc4e162ed3d3d1a2d636df78cbd81c2e40ad9a
Cartel-80 <joshuamulenga96@hotmail.com>
2024-03-02 22:32:31 -0400

    initial commit

---
+++ src/main/java/com/j11a/sakilapayment/SakilaPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.j11a.sakilapayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SakilaPaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/j11a/sakilapayment/SakilaPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j11a.sakilapayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
